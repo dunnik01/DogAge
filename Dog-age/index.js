c27c6831b77683d285cc37635a64a39a28c4a815
dunnik01 <72144323+dunnik01@users.noreply.github.com>
2020-11-06 20:27:09 +0530

    Add files via upload

---
+++ Dog-age/index.js
@@ -0,0 +1,23 @@
+
+
+$("button").click(function(){
+logic();
+});
+
+$(".input").keydown(function(event ) {
+  if ( event.which == 13 ) {
+    logic();
+  }
+});
+function logic(){
+
+  var dogAge= $(".input").val();
+  if(!$.isNumeric(dogAge)){
+    alert("Please enter a numeric value");
+  }
+  if(dogAge>28){
+    alert("Waaw!!! Your dog is the oldest dog ever alive. Please apply for guinness record!!!");
+  }
+  var humanAge = (dogAge-2)*4 + 21;
+  $(".human-age").text(" Your dog's age is equivalent to "+humanAge + " human years");
+}
